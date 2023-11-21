bdb8492fceb165a955d92d466fb7eb48a08e0e11
quynguy <wquyenle@gmail.com>
2023-11-21 17:19:14 -0600

    created essential app folders and loaded pages

---
+++ miniminder_app/server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+
+// initialize express app // 
+const app = express();
+
+
+// landing page // 
+app.get('/', function (req,res) {
+    res.send("Miniminder Application");
+});
+
+// home page // 
+app.get('/home', function (req,res) {
+    res.send("Home Page");
+    console.log("Home Page Loaded")
+});
+
+// page - admin dashboard //
+app.get('/admin', function (req,res) {
+    res.send("Admin Page");
+    console.log("Admin Page Only Loaded")
+});
+
+// page - parents dashboard //
+app.get('/parents', function (req,res) {
+    res.send("Parents Page");
+    console.log("Parents Only Loaded")
+});
+
+
+// page - admin post //
+// admin privilege //
+app.get('/createpost', function (req,res) {
+    res.send("Create Post - Admins Only");
+    console.log("Create Post Page Loaded")
+});
+
+
+
+// make a comment on a post // 
+app.get('/postcomment', function (req,res) {
+    res.send("Posts - Comment Page");
+    console.log("Comment Page Loaded")
+});
+
+
+// admin - create staff // 
+app.get('/create-staff', function (req,res) {
+    res.send("Create New Staff Profile");
+    console.log("Create Staff Page Loaded")
+});
+
+// admin - create child // 
+app.get('/create-child', function (req,res) {
+    res.send("Create New Child Profile");
+    console.log("Create Child Page Loaded")
+});
+
+
+// admin - update child // 
+app.get('/update-child', function (req,res) {
+    res.send("UpdateChild Profile");
+    console.log("Update Child Page Loaded")
+});
+
+
+
+
+
+app.listen(1616, function () {
+    console.log(`Server is running on http://localhost:$(1616)`);
+});
+
